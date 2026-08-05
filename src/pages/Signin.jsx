2148807139aaa79d56b34a2666221ec1b3ba8393
Misskhat <misskhat.hossen@gmail.com>
2026-08-05 17:59:54 +0600

    signin and singout logic implement and ui set

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,5 +1,8 @@
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { useState } from "react";
 import { Link } from "react-router";
+import { auth } from "../firebase/firebase.config";
+import { toast } from "react-toastify";
 
 function Signin() {
   const [show, setShow] = useState(true);
@@ -10,6 +13,29 @@ function Signin() {
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log({ email, password });
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log(res.user);
+        toast.success("Successfully sign in");
+        setUser(res.user);
+      })
+      .catch((e) => {
+        const errorMessage = e.message;
+        console.log(errorMessage);
+        toast.error(errorMessage);
+      });
+  };
+
+  const handleSignOut = () => {
+    console.log("signout");
+    signOut(auth)
+      .then(() => {
+        toast.success("Successfully signOUt");
+        setUser(null);
+      })
+      .catch((e) => {
+        toast.error(e.message);
+      });
   };
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#90CAF9] via-[#2196F3] to-[#90CAF9]">
@@ -24,79 +50,98 @@ function Signin() {
             </p>
           </div>
           <div className="card bg-base-100/50 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <form onSubmit={handleSignIn} className="fieldset">
-                <label className="label">Email</label>
-                <input
-                  name="email"
-                  type="email"
-                  className="input"
-                  placeholder="Email"
-                />
-                <div className="relative">
-                  <label className="label">Password</label>
-                  <input
-                    type={show ? "password" : "text"}
-                    className="input"
-                    placeholder="Password"
-                    name="password"
-                  />
-                  <span
-                    onClick={() => setShow(!show)}
-                    className="absolute text-xl right-6 bottom-[8px] cursor-pointer"
-                  >
-                    {show ? "☺️" : "🙂"}
-                  </span>
-                </div>
-                <a className="link link-hover">Forgot password?</a>
-
-                <button className="btn text-white mt-4 bg-gradient-to-br from-[#90CAF9] via-[#2196F3] to-[#90CAF9]">
-                  SignIN
-                </button>
-                {/* Divider */}
-                <div className="flex items-center justify-center gap-2 my-2">
-                  <div className="w-16 h-px bg-gray-800/50"></div>
-                  <div className="text-white/90">Or</div>
-                  <div className="w-16 h-px bg-gray-800/50"></div>
-                </div>
-                <button className="btn bg-white text-black border-[#e5e5e5]">
-                  <svg
-                    aria-label="Google logo"
-                    width="16"
-                    height="16"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                  >
-                    <g>
-                      <path d="m0 0H512V512H0" fill="#fff"></path>
-                      <path
-                        fill="#34a853"
-                        d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                      ></path>
-                      <path
-                        fill="#4285f4"
-                        d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                      ></path>
-                      <path
-                        fill="#fbbc02"
-                        d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                      ></path>
-                      <path
-                        fill="#ea4335"
-                        d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                      ></path>
-                    </g>
-                  </svg>
-                  Login with Google
-                </button>
-                <p className="text-center">
-                  If you have an account please{" "}
-                  <Link to={"/sign-up"} className="text-[#2196F3] font-bold">
-                    Singup
-                  </Link>
+            {user ? (
+              <div className="card-body">
+                <p className="text-center font-bold text-2xl">
+                  User Email: {user.email}
                 </p>
-              </form>
-            </div>
+                <button
+                  onClick={handleSignOut}
+                  className="text-[#2196F3] font-bold bg-white p-2 rounded-xl"
+                >
+                  SignOut
+                </button>
+              </div>
+            ) : (
+              <>
+                <div className="card-body">
+                  <form onSubmit={handleSignIn} className="fieldset">
+                    <label className="label">Email</label>
+                    <input
+                      name="email"
+                      type="email"
+                      className="input"
+                      placeholder="Email"
+                    />
+                    <div className="relative">
+                      <label className="label">Password</label>
+                      <input
+                        type={show ? "password" : "text"}
+                        className="input"
+                        placeholder="Password"
+                        name="password"
+                      />
+                      <span
+                        onClick={() => setShow(!show)}
+                        className="absolute text-xl right-6 bottom-[8px] cursor-pointer"
+                      >
+                        {show ? "☺️" : "🙂"}
+                      </span>
+                    </div>
+                    <a className="link link-hover">Forgot password?</a>
+
+                    <button className="btn text-white mt-4 bg-gradient-to-br from-[#90CAF9] via-[#2196F3] to-[#90CAF9]">
+                      SignIN
+                    </button>
+                    {/* Divider */}
+                    <div className="flex items-center justify-center gap-2 my-2">
+                      <div className="w-16 h-px bg-gray-800/50"></div>
+                      <div className="text-white/90">Or</div>
+                      <div className="w-16 h-px bg-gray-800/50"></div>
+                    </div>
+                    <button className="btn bg-white text-black border-[#e5e5e5]">
+                      <svg
+                        aria-label="Google logo"
+                        width="16"
+                        height="16"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <g>
+                          <path d="m0 0H512V512H0" fill="#fff"></path>
+                          <path
+                            fill="#34a853"
+                            d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                          ></path>
+                          <path
+                            fill="#4285f4"
+                            d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                          ></path>
+                          <path
+                            fill="#fbbc02"
+                            d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                          ></path>
+                          <path
+                            fill="#ea4335"
+                            d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                          ></path>
+                        </g>
+                      </svg>
+                      Login with Google
+                    </button>
+                    <p className="text-center">
+                      If you have an account please{" "}
+                      <Link
+                        to={"/sign-up"}
+                        className="text-[#2196F3] font-bold"
+                      >
+                        Singup
+                      </Link>
+                    </p>
+                  </form>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
